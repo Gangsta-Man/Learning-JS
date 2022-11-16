109d1a41beb61dc80c2461ab349a17d6548c05ab
Musa <musa.hutiev95@gmail.com>
2022-11-16 23:54:25 +0300

    js learning

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+let lastMovie = prompt('Один из последних просмотренных фильмов?'),
+		rating = prompt('На сколько оцените его?'),
+		lastMovie2 = prompt('Один из последних просмотренных фильмов?'),
+		rating2 = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[lastMovie] = rating;
+personalMovieDB.movies[lastMovie2] = rating2;
+
+
+console.log(personalMovieDB);
